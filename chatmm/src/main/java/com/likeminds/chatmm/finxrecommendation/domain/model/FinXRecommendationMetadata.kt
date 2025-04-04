febb96b42dca5cc4c73e1f08b1659039b2d9bf23
Ganesh Swami <ganesh.swami@choicetechlab.com>
2025-04-04 12:29:32 +0530

    initial reel integration commit

--- chatmm/src/main/java/com/likeminds/chatmm/finxrecommendation/domain/model/FinXRecommendationMetadata.kt
+++ chatmm/src/main/java/com/likeminds/chatmm/finxrecommendation/domain/model/FinXRecommendationMetadata.kt
@@ -15,5 +15,13 @@ data class FinXRecommendationMetadata(
 
     //Scrip Data
     @SerializedName("searchRsp") val searchRsp: FinxSmSearchApiRsp? = null,
-    @SerializedName("customWidgetType") val customWidgetType: String? = ""
+    @SerializedName("customWidgetType") val customWidgetType: String? = "",
+
+    //MF-Scheme Data
+    @SerializedName("mfSchemeCode") val mfSchemeCode: Int? = 0,
+    @SerializedName("mfSchemePlanCode") val mfSchemePlanCode: Int? = 0,
+
+    //Redirection Type
+    @SerializedName("redirectionTypeOrderPlace") val redirectionTypeOrderPlace: String? = "",
+    @SerializedName("redirectionTypeCompanyPage") val redirectionTypeCompanyPage: String? = "",
 ) : Parcelable
